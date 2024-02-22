39b810fe359e7d9bf3719e4cd8102d24161308e6
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2024-02-22 11:12:21 +0600

    load cart product

--- index.js
+++ index.js
@@ -60,6 +60,10 @@ async function run() {
         const result =await productCollection.find().toArray();
         res.send(result);
     })
+    app.get('/myCart',async(req,res)=>{
+        const result =await myCartCollection.find().toArray();
+        res.send(result);
+    })
     app.get('/products/:id',async(req,res)=>{
         const id =req.params.id;
         const query={_id: new ObjectId(id)}
