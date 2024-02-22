e55f382ec17c8a8f487262c865354148cd3a0958
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2024-02-22 09:42:36 +0600

    load single product

--- index.js
+++ index.js
@@ -52,6 +52,12 @@ async function run() {
         const result =await productCollection.find().toArray();
         res.send(result);
     })
+    app.get('/products/:id',async(req,res)=>{
+        const id =req.params.id;
+        const query={_id: new ObjectId(id)}
+        const result =await productCollection.findOne(query);
+        res.send(result);
+    })
 
 
 
